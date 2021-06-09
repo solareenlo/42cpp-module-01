f79299c9a99f229c17b496af76f898333d64b0c1
solareenlo <solareenlo@gmail.com>
2021-06-09 20:38:37 +0900

    fix: ex05

--- ex05/Brain.cpp
+++ ex05/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 04:04:03 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/05/26 04:47:30 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/06/09 20:38:21 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ std::string    Brain::identify(void) const {
 
 std::string Brain::toupper_(std::string str) const {
     std::string res = "";
-    for (int i = 0; str[i]; i++)
+    for (int i = 2; str[i]; i++)
         res += toupper(str[i]);
     return (res);
 }
